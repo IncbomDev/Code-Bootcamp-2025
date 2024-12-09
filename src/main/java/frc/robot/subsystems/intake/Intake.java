0da4ca48dfd75e294d82c42b1c0353228cbcb848
Sparsh <gokuvsninja@gmail.com>
2024-12-09 15:36:56 -0500

    Completed Intake Subsystem

--- src/main/java/frc/robot/subsystems/intake/Intake.java
+++ src/main/java/frc/robot/subsystems/intake/Intake.java
@@ -1,42 +1,35 @@
 package frc.robot.subsystems.intake;
 
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class intake extends SubsystemBase {
+public class Intake extends SubsystemBase {
+  private static final int CAN_ID = 1;
+  private final IntakeIOTalonFX intakeMotor;
+  private final IntakeIOInputsAutoLogged feedMotorInputs = new IntakeIOInputsAutoLogged();
 
-    private final IntakeIOTalonFX intakeMotor;
-    private final IntakeIOInputsAutoLogged feedMotorInputs = new IntakeIOInputsAutoLogged();
+  public Intake() {
+    intakeMotor = new IntakeIOTalonFX(CAN_ID); // To Do: Add Motor ID inside the ()
+    this.disableIntake();
+  }
 
-    public void setSpeed(double speed){}
+  @Override
+  public void periodic() {
+    intakeMotor.updateInputs(feedMotorInputs);
+  }
 
-    private double feedSetSpeed = 10.0;
+  public void startIntake() {
+    intakeMotor.setSpeed(1.0);
+  }
 
-    public Intake(){
-        intakeMotor = new IntakeIOTalonFX() // To Do: Add Motor ID inside the ()
-        this.disableIntake();
-    }
+  public void reverseIntake() {
+    intakeMotor.setSpeed(-5.0); // To Do: Find Better Values
+  }
 
-    @Override
-    public void periodic(){
-        double speed = robotSpeed.vxMetersPerSecond;
-        feedMotor.updateInputs(feedMotorInputs, speed);
-    }
+  public void forwardIntake() {
+    intakeMotor.setSpeed(5.0);
+  }
 
-    public void startIntake(){
-        intake.setSpeed(1.0);
-    }
-
-    public void reverseIntake(){
-        intake.setSpeed(-5.0); // To Do: Find Better Values
-    }
-
-    public void forwardIntake(){
-        intake.setSpeed(5.0);
-    }
-
-    public void disableIntake(){
-        intake.setSpeed(0.0);
-    }
-
+  public void disableIntake() {
+    intakeMotor.setSpeed(0.0);
+  }
 }
--- src/main/java/frc/robot/subsystems/intake/IntakeIO.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeIO.java
@@ -1,15 +1,14 @@
 package frc.robot.subsystems.intake;
+
 import org.littletonrobotics.junction.AutoLog;
 
 public interface IntakeIO {
-    @AutoLog
-    public static class IntakeIOInputs {
-        public double motorVelocityRotationsPerSecond = 0.0d;
-        public double setPoint = 0.0d;
-        public double setPointError = 0.0d;
+  @AutoLog
+  public static class IntakeIOInputs {
+    public double motorVelocityRotationsPerSecond = 0.0d;
+  }
 
-    }
+  public default void updateInputs(IntakeIOInputs inputs) {}
 
-    public default void updateInputs(IntakeIOInputs inputs, double speed){}
-    public default void setSpeed(double speed);
+  public default void setSpeed(double speed) {}
 }
